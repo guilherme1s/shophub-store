6dc5459d6c10131fe8c953d0b882e7c9e4bb2aa3
guilherme1s <2000guilherme.silva@gmail.com>
2025-11-17 20:37:51 -0300

    [ADD] Configuração inicial das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Cart } from "./pages/cart";
+import { Catalog } from "./pages/catalog";
+
 export function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogo" element={<Catalog />} />
+          <Route path="/carrinho" element={<Cart />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
---
+++ src/pages/catalog/index.tsx
@@ -0,0 +1,5 @@
+export function Catalog() {
+    return (
+        <h1>Catalog page</h1>
+    );
+}
