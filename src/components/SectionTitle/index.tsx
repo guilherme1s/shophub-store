4706e7ac4b3ff37057b7ca44b716a8ccdd71b785
guilherme1s <2000guilherme.silva@gmail.com>
2025-12-17 20:56:26 -0300

    [MOD] Atualiza componente SectionTitle

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -5,13 +5,13 @@ interface SectionTitleProps {
 
 export function SectionTitle({ title, description }: SectionTitleProps) {
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <div className="flex items-start gap-2">
         <span className="w-1 h-8 xl:h-9 bg-primary rounded-full" />
         <h1 className="text-3xl xl:text-4xl font-bold">{title}</h1>
       </div>
 
-      {description && <p className="text-gray-600">{description}</p>}
+      {description && <p className="text-gray-600 text-xl">{description}</p>}
     </div>
   );
 }
