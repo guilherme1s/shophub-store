a124a3101e101e39493492ba3231377422a6f080
guilherme1s <2000guilherme.silva@gmail.com>
2025-12-19 20:05:42 -0300

    [ADD] Cria layout da pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,42 @@
-import { Footer } from "../../components/Footer";
-import { Header } from "../../components/Header";
+import { Hero } from "../../pages/home/components/Hero";
+import { ProductCard } from "../../components/ProductCard";
+import { PromoSection } from "../../pages/home/components/PromoSection";
+import { SectionTitle } from "../../components/SectionTitle";
+import { ShopCategory } from "./components/ShopCategory";
+import { categories } from "../../data/categories";
+
+import { Container } from "../../components/Container";
 
 export function Home() {
   return (
-    <div>
-      <Header />
-      <h1>Home page</h1>
-      <Footer />
-    </div>
+    <main>
+      <Hero src="#" />
+
+      <section className="py-8 mb-20 border-b border-gray-300">
+        <Container>
+          <PromoSection />
+        </Container>
+      </section>
+
+      <Container>
+        <section>
+          <SectionTitle
+            title="Mais vendidos"
+            description="Navegue pelos itens mais vendidos do momento"
+          />
+
+          <div className="mt-10 grid gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <ProductCard
+              src=""
+              title="Produto Exemplo"
+              price={200}
+              rating={4.5}
+            />
+          </div>
+        </section>
+      </Container>
+
+      <ShopCategory content={categories} title="Compre por categoria" />
+    </main>
   );
 }
