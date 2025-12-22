dcd6ce692989036a3e560bbe7cc2aa60e0af0134
guilherme1s <2000guilherme.silva@gmail.com>
2025-12-22 09:30:58 -0300

    [FIX] Correção no espaçamento do componente Container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,6 +4,6 @@ interface ContainerProps {
 
 export function Container({ children }: ContainerProps) {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-0">{children}</div>
   );
 }
